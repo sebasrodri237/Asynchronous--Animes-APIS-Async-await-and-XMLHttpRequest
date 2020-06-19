dc0e730a29476ff1b1d09898fdda76d770316350
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-19 00:14:28 -0500

    The first practice with asynchronous and xmlhtttprequest

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-const fetchData = require('utils/fetchData');
+const fetchData = require('C:/Users/Hp/Desktop/Cursos programacion/Javascript Practices/Asynchronous- Animes APIS, Async-await and XMLHttpRequest/utils/fetchData');
 //Require the module fetchData to make the request, (it's only a external function)
 const API = 'https://ghibliapi.herokuapp.com/films/';
 //URL to get The Studio Ghibli Films
@@ -8,12 +8,15 @@ const asyncFunction = async url_api => {
     try{
     //Try to get the request and the data
         const movie = await fetchData(url_api);
-        //
+        //The instruccion await pause the async function and wait
+        //to resolve the promise. First request
         const character = await fetchData(`${movie[8].people[0]}`);
-        const specie = await fetchData(character.name);
-
+        //Second request never happen if the first request is not done
+        const specie = await fetchData(`${character.species}`);
+        //Third request never happen if the second request is not done
+        // This code is asynchronous but behaves as synchronous
         console.log(`The name of the movie is: ${movie[8].title}`);
-        console.log(`${character[8].people[0]}`);
+        console.log(`The main character is: ${character.name}`);
         console.log(`His species is: ${specie.name}`);
 
     }catch(error){
@@ -22,7 +25,5 @@ const asyncFunction = async url_api => {
     }
 }
 
-console.log('Before');
 asyncFunction(API);
-console.log('After');
 
